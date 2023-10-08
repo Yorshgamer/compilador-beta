83c8d0d585843c496be9394a031769c8ae9da182
Yorshgamer <146681194+Yorshgamer@users.noreply.github.com>
2023-10-08 10:02:30 -0500

    Add files via upload

---
+++ Compilador binario/Compilador binario/Form1.Designer.cs
@@ -0,0 +1,531 @@
+﻿namespace Compilador_binario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Nemonico = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblHex = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(515, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CREAR ARCHIVO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.CrearArchivo_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(634, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SALIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Sali_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(468, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(247, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(89, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(512, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TITULO DEL ARCHIVO";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(468, 148);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(230, 147);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(555, 319);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 8;
+            this.btnLeer.Text = "LEER";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(455, 379);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(105, 39);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nemonico";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Operando";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Direccion";
+            // 
+            // Nemonico
+            // 
+            this.Nemonico.FormattingEnabled = true;
+            this.Nemonico.Items.AddRange(new object[] {
+            "",
+            "NOT",
+            "NAND",
+            "ADD",
+            "LDA",
+            "OUTA",
+            "OUTB",
+            "INA",
+            "RD",
+            "RA",
+            "LDRA",
+            "JPI",
+            "JPC",
+            "JPZ"});
+            this.Nemonico.Location = new System.Drawing.Point(146, 55);
+            this.Nemonico.Name = "Nemonico";
+            this.Nemonico.Size = new System.Drawing.Size(72, 24);
+            this.Nemonico.TabIndex = 13;
+            this.Nemonico.SelectedIndexChanged += new System.EventHandler(this.Nemonico_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(374, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "HEX";
+            // 
+            // lblHex
+            // 
+            this.lblHex.AutoSize = true;
+            this.lblHex.Location = new System.Drawing.Point(377, 57);
+            this.lblHex.Name = "lblHex";
+            this.lblHex.Size = new System.Drawing.Size(39, 16);
+            this.lblHex.TabIndex = 15;
+            this.lblHex.Text = "Valor";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "0A",
+            "0B",
+            "0C",
+            "0D",
+            "0E",
+            "0F",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "1A",
+            "1B",
+            "1C",
+            "1D",
+            "1E",
+            "1F",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "2A",
+            "2B",
+            "2C",
+            "2D",
+            "2E",
+            "2F",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "3A",
+            "3B",
+            "3C",
+            "3D",
+            "3E",
+            "3F",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "4A",
+            "4B",
+            "4C",
+            "4D",
+            "4E",
+            "4F",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "5A",
+            "5B",
+            "5C",
+            "5D",
+            "5E",
+            "5F",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "6A",
+            "6B",
+            "6C",
+            "6D",
+            "6E",
+            "6F",
+            "70",
+            "71",
+            "72",
+            "73",
+            "74",
+            "75",
+            "76",
+            "77",
+            "78",
+            "79",
+            "7A",
+            "7B",
+            "7C",
+            "7D",
+            "7E",
+            "7F",
+            "80",
+            "81",
+            "82",
+            "83",
+            "84",
+            "85",
+            "86",
+            "87",
+            "88",
+            "89",
+            "8A",
+            "8B",
+            "8C",
+            "8D",
+            "8E",
+            "8F",
+            "90",
+            "91",
+            "92",
+            "93",
+            "94",
+            "95",
+            "96",
+            "97",
+            "98",
+            "99",
+            "9A",
+            "9B",
+            "9C",
+            "9D",
+            "9E",
+            "9F",
+            "A0",
+            "A1",
+            "A2",
+            "A3",
+            "A4",
+            "A5",
+            "A6",
+            "A7",
+            "A8",
+            "A9",
+            "AA",
+            "AB",
+            "AC",
+            "AD",
+            "AE",
+            "AF",
+            "B0",
+            "B1",
+            "B2",
+            "B3",
+            "B4",
+            "B5",
+            "B6",
+            "B7",
+            "B8",
+            "B9",
+            "BA",
+            "BB",
+            "BC",
+            "BD",
+            "BE",
+            "BF",
+            "C0",
+            "C1",
+            "C2",
+            "C3",
+            "C4",
+            "C5",
+            "C6",
+            "C7",
+            "C8",
+            "C9",
+            "CA",
+            "CB",
+            "CC",
+            "CD",
+            "CE",
+            "CF",
+            "D0",
+            "D1",
+            "D2",
+            "D3",
+            "D4",
+            "D5",
+            "D6",
+            "D7",
+            "D8",
+            "D9",
+            "DA",
+            "DB",
+            "DC",
+            "DD",
+            "DE",
+            "DF",
+            "E0",
+            "E1",
+            "E2",
+            "E3",
+            "E4",
+            "E5",
+            "E6",
+            "E7",
+            "E8",
+            "E9",
+            "EA",
+            "EB",
+            "EC",
+            "ED",
+            "EE",
+            "EF",
+            "F0",
+            "F1",
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6",
+            "F7",
+            "F8",
+            "F9",
+            "FA",
+            "FB",
+            "FC",
+            "FD",
+            "FF"});
+            this.comboBox1.Location = new System.Drawing.Point(36, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(78, 24);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(36, 148);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(318, 147);
+            this.richTextBox2.TabIndex = 17;
+            this.richTextBox2.Text = "";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(111, 104);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(148, 38);
+            this.btnAgregar.TabIndex = 18;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblHex);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Nemonico);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Nemonico;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblHex;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
---
+++ Compilador binario/Compilador binario/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Compilador_binario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+
+        private void CrearArchivo_Click(object sender, EventArgs e)
+        {
+            {
+                string nombre = textBox1.Text;
+                StreamWriter escribir
+                     = new StreamWriter(@"C:\Users\WINDOWS11\Desktop\"+ nombre + ".bin", true);
+                try
+                {
+                    escribir.WriteLine(richTextBox2.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Error");                
+                }
+                escribir.Close();
+            }
+        }
+
+        private void Sali_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text;
+            StreamReader leer = new StreamReader(@"C:\Users\WINDOWS11\Desktop\" + nombre + ".bin");
+            string linea;
+            try
+            {
+                linea = leer.ReadLine();
+                while (linea != null)
+                {
+                    richTextBox1.AppendText(linea+ "\n");
+                    linea = leer.ReadLine();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            richTextBox1.Clear();
+        }
+
+        public void Nemonico_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = Nemonico.SelectedIndex;
+            if (indice == 1) //NOT
+            {
+                lblHex.Text = "00";
+            }else
+                if (indice ==2)//NAND
+            {
+                    lblHex.Text = "1"+textBox2.Text;
+                }else
+                if (indice==3)//ADD
+            {
+                    lblHex.Text = "2" + textBox2.Text;
+                }else
+                if(indice == 4)//LDA
+            {
+                    lblHex.Text = "3" + textBox2.Text;
+                }
+                else
+                if (indice == 5)//OUTA
+            {
+                    lblHex.Text = "40";
+                }
+                else
+                if (indice == 6)//OUTB
+            {
+                    lblHex.Text = "50";
+                }
+                else
+                if (indice == 7)//INA
+            {
+                    lblHex.Text = "60";
+                }
+            else
+                if (indice == 8)//RD
+            {
+                lblHex.Text = "70";
+            }
+            else
+                if (indice == 9)//RA
+            {
+                lblHex.Text = "80";
+            }
+            else
+                if (indice == 10)//LDRA
+            {
+                lblHex.Text = "90";
+            }
+            else
+                if (indice == 11)//JPI
+            {
+                lblHex.Text = "C"+textBox2.Text;
+            }
+            else
+                if (indice == 12)//JPC
+            {
+                lblHex.Text = "D"+textBox2.Text;
+            }
+            else
+                if (indice == 13)//JPZ
+            {
+                lblHex.Text = "E"+textBox2.Text;
+            }
+        }
+
+        public class HexadecimalConverter
+        {
+            public static string ToASCII(string hex)
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hex.Length; i += 2)
+                {
+                    ascii += (char)ToDecimal(hex.Substring(i, 2));
+                }
+
+                return ascii;
+            }
+            public static int ToDecimal(string hex)
+            {
+                hex = hex.ToUpper();
+
+                int hexLength = hex.Length;
+                double dec = 0;
+
+                for (int i = 0; i < hexLength; ++i)
+                {
+                    byte b = (byte)hex[i];
+
+                    if (b >= 48 && b <= 57)
+                        b -= 48;
+                    else if (b >= 65 && b <= 70)
+                        b -= 55;
+
+                    dec += b * Math.Pow(16, ((hexLength - i) - 1));
+                }
+
+                return (int)dec;
+            }
+        }
+        public void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string hexa = lblHex.Text;
+            StringReader obtDats = new StringReader(hexa);
+            
+            string linea2;
+            string Convert;
+            try
+            {
+                linea2 = obtDats.ReadLine();
+                Convert = HexadecimalConverter.ToASCII(linea2);
+                richTextBox2.AppendText(Convert );
+                   
+                
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+
+        }
+    }
+}
